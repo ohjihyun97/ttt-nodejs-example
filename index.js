27d1471baa2f23739c46545242773b6a86bd2157
ohjihyun97 <37072277+ohjihyun97@users.noreply.github.com>
2018-05-15 14:43:07 +0900

    수정

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ app.use(cors()); // http://guswnsxodlf.github.io/enable-CORS-on-express
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var gameState = { isXnext: true, squares: Array(9).fill('') };
+var gameState = { winner: '', isXnext: true, squares: Array(9).fill('') };
+
+function wins(turn){ //turn='X' 또는 ruturn='O'
+  var speaker = gameState.squares
+
+  return (speaker[0]==turn && speaker[1]==turn && speaker[2]==turn) ||
+    (speaker[3]==turn && speaker[4]==turn && speaker[5]==turn) ||
+    (speaker[6]==turn && speaker[7]==turn && speaker[8]==turn) ||
+    (speaker[0]==turn && speaker[3]==turn && speaker[6]==turn) ||
+    (speaker[1]==turn && speaker[4]==turn && speaker[7]==turn) ||
+    (speaker[3]==turn && speaker[5]==turn && speaker[9]==turn) ||
+    (speaker[0]==turn && speaker[4]==turn && speaker[8]==turn) ||
+    (speaker[2]==turn && speaker[4]==turn && speaker[6]==turn) ;
+
+
+function caculwinner(){
+//x가 이기는 경우
+//o가 이기는 경우
+  return '';
+}
 
 app.get('/game_state', (req, res) => {
   res.charset = 'UTF-8';
@@ -19,10 +38,17 @@ app.get('/move', (req, res) => {
   res.charset = 'UTF-8';
   var turn = req.query.turn;
   var pos = req.query.pos;
-  console.log('move: '+turn+pos);
-  gameState.squares[pos] = turn;
-  gameState.isXnext = !gameState.isXnext;
-  res.send('OK');
+  //맞는 조건 검사
+  if( gameState.isXnext ? (turn == 'X') : (turn == 'O')){
+    //(gameState.isXnext && turn == 'X') || (!gameState.isXnext && turn == 'O')){
+  //  console.log('move: '+turn+pos);
+    gameState.squares[pos] = turn;
+    gameState.isXnext = !gameState.isXnext;
+    res.send('OK');
+  }
+  else {
+    res.send('ERROR');
+  }
 } );
 
 app.get('/', (req, res) => {
